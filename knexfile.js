af0f9f7adee0e65c51a09f3b3cefc2f1cae603cd
verydecent <wnjhwng@gmail.com>
2019-06-27 12:53:23 -0700

    Add directories for migrations and seeds

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,15 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/data.sqlite3'
     },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+    
     useNullAsDefault: true,
   },
 
