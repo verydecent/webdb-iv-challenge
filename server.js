e989818af321cc5c9d1772e0fe9312c741a3b4f0
verydecent <wnjhwng@gmail.com>
2019-06-27 12:57:53 -0700

    Sanity check get request success

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ server.use(express.json());
 
 const port = process.env.PORT || 5000;
 
+server.get('/', (req, res) => {
+  res.status(200).json({ message: "Sanity check" });
+});
+
 server.listen(port, () => console.log(` === Server live on Port ${port} === `));
